03f907b2b542a3abb3cc0984ef3ff28833de538d
noblespirit <janne.karjalainen@aalto.fi>
2017-01-05 12:00:59 +0200

    .

--- sattisite/settings.py
+++ sattisite/settings.py
@@ -136,10 +136,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    )
-
 LOGIN_URL = '/login/'
 
 CHANNEL_LAYERS = {
