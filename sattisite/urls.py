425c2fd9f06a91ee463831a23a4a879db9dc5ff6
noblespirit <janne.karjalainen@aalto.fi>
2017-01-05 12:09:20 +0200

    .

--- sattisite/urls.py
+++ sattisite/urls.py
@@ -48,10 +48,3 @@ urlpatterns = [
 urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-if conf.settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
