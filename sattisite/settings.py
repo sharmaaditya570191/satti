72202f92010c52ef567e8034bc7ae1260e65cadc
noblespirit <janne.karjalainen@aalto.fi>
2017-02-08 09:39:58 +0200

    debug off

--- sattisite/settings.py
+++ sattisite/settings.py
@@ -28,7 +28,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'lgrc@(m@3cc*&6r)yeki+209#6#ql4#3ura31fczs3=f2d#pn$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['morning-meadow-59619.herokuapp.com']
 
